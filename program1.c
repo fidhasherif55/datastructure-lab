0cc3740b42618719cc33c0cd5769f845a794c56a
fidhasherif55 <sheriffidha633@gmail.com>
2025-10-14 12:42:48 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int l,b,a,p;
+printf("enter the length and breadth of rectangle");
+scanf("%d,%d",&l,$b);
+a=l*b;
+p=2*l+b;
+printf("area of rectangle is %d%d",a,b);
+
+}
